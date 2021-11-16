mezz/JustEnoughItems

Fix #2563: Clicking ingredients in displayed recipes does not work

src/main/java/mezz/jei/gui/GuiScreenHelper.java
@@ -161,7 +161,12 @@ public class GuiScreenHelper {
 			Rectangle2d area = null;
 			Slot slotUnderMouse = guiContainer.getSlotUnderMouse();
 			if (ingredient instanceof ItemStack && slotUnderMouse != null && ItemStack.matches(slotUnderMouse.getItem(), (ItemStack) ingredient)) {
-				area = new Rectangle2d(slotUnderMouse.x, slotUnderMouse.y, 16, 16);
+				area = new Rectangle2d(
+						guiContainer.getGuiLeft() + slotUnderMouse.x,
+						guiContainer.getGuiTop() + slotUnderMouse.y,
+						16,
+						16
+				);
 			}
 			return ClickedIngredient.create(ingredient, area);
 		}

src/main/java/mezz/jei/gui/recipes/RecipesGui.java
@@ -293,12 +293,19 @@ public class RecipesGui extends Screen implements IRecipesGui, IShowsRecipeFocus
 			}
 
 			if (isMouseOver(mouseX, mouseY)) {
-				for (RecipeLayout<?> recipeLayouts : this.recipeLayouts) {
-					GuiIngredient<?> clicked = recipeLayouts.getGuiIngredientUnderMouse(mouseX, mouseY);
+				for (RecipeLayout<?> recipeLayout : this.recipeLayouts) {
+					GuiIngredient<?> clicked = recipeLayout.getGuiIngredientUnderMouse(mouseX, mouseY);
 					if (clicked != null) {
 						Object displayedIngredient = clicked.getDisplayedIngredient();
 						if (displayedIngredient != null) {
-							return ClickedIngredient.create(displayedIngredient, clicked.getRect());
+							Rectangle2d area = clicked.getRect();
+							area = new Rectangle2d(
+									area.getX() + recipeLayout.getPosX(),
+									area.getY() + recipeLayout.getPosY(),
+									area.getWidth(),
+									area.getHeight()
+							);
+							return ClickedIngredient.create(displayedIngredient, area);
 						}
 					}
 				}

Mysticpasta1/JEI-Async

src/main/java/mezz/jei/gui/GuiScreenHelper.java
@@ -159,7 +159,12 @@ public class GuiScreenHelper {
 			Rect2i area = null;
 			Slot slotUnderMouse = guiContainer.getSlotUnderMouse();
 			if (ingredient instanceof ItemStack && slotUnderMouse != null && ItemStack.matches(slotUnderMouse.getItem(), (ItemStack) ingredient)) {
-				area = new Rect2i(slotUnderMouse.x, slotUnderMouse.y, 16, 16);
+				area = new Rect2i(
+					guiContainer.getGuiLeft() + slotUnderMouse.x,
+					guiContainer.getGuiTop() + slotUnderMouse.y,
+					16,
+					16
+				);
 			}
 			return ClickedIngredient.create(ingredient, area);
 		}

src/main/java/mezz/jei/gui/recipes/RecipesGui.java
@@ -291,12 +291,19 @@ public class RecipesGui extends Screen implements IRecipesGui, IShowsRecipeFocus
 			}
 
 			if (isMouseOver(mouseX, mouseY)) {
-				for (RecipeLayout<?> recipeLayouts : this.recipeLayouts) {
-					GuiIngredient<?> clicked = recipeLayouts.getGuiIngredientUnderMouse(mouseX, mouseY);
+				for (RecipeLayout<?> recipeLayout : this.recipeLayouts) {
+					GuiIngredient<?> clicked = recipeLayout.getGuiIngredientUnderMouse(mouseX, mouseY);
 					if (clicked != null) {
 						Object displayedIngredient = clicked.getDisplayedIngredient();
 						if (displayedIngredient != null) {
-							return ClickedIngredient.create(displayedIngredient, clicked.getRect());
+							Rect2i area = clicked.getRect();
+							area = new Rect2i(
+									area.getX() + recipeLayout.getPosX(),
+									area.getY() + recipeLayout.getPosY(),
+									area.getWidth(),
+									area.getHeight()
+							);
+							return ClickedIngredient.create(displayedIngredient, area);
 						}
 					}
 				}